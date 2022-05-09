d162148f64dc158a7dee36df25715ab344242920
DaneOnishi <daniellaonishi07@gmail.com>
2022-05-09 14:38:02 -0300

    Initial Commit

---
+++ Rinha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rinha
+//
+//  Created by Daniella Onishi on 09/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rinha/RinhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RinhaApp.swift
+//  Rinha
+//
+//  Created by Daniella Onishi on 09/05/22.
+//
+
+import SwiftUI
+
+@main
+struct RinhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
