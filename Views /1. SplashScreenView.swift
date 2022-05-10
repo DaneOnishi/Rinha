26f3343422ae62c2afdfebc7c2e226c2ffa3d99f
DaneOnishi <daniellaonishi07@gmail.com>
2022-05-09 23:25:11 -0300

    Starts design system and adds splash screen and menu views

---
+++ Views /1. SplashScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashScreenView.swift
+//  Rinha
+//
+//  Created by Daniella Onishi on 09/05/22.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
