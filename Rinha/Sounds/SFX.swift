f7124f3dd6a0cc9e4f1bc3dd34cdf0fa83df9e10
DaneOnishi <daniellaonishi07@gmail.com>
2022-05-09 22:51:00 -0300

    Adds assets and soundtrack

---
+++ Rinha/Sounds/SFX.swift
@@ -0,0 +1,88 @@
+//
+//  SFX.swift
+//  Rinha
+//
+//  Created by Daniella Onishi on 09/05/22.
+//
+
+import Foundation
+import AVFoundation
+
+class SFXMusicSingleton: NSObject, AVAudioPlayerDelegate {
+    static let shared = SFXMusicSingleton()
+    fileprivate var currentPlayer: AVAudioPlayer?
+    private override init() {}
+    
+    var players: [URL: AVAudioPlayer] = [:]
+    var duplicatePlayers: [AVAudioPlayer] = []
+    
+    func playSound(soundFileName: String, loop: Bool) {
+        
+        guard let bundle = Bundle.main.path(forResource: soundFileName, ofType: "mp3")
+        else{ return }
+        
+        let soundFileNameURL = URL(fileURLWithPath: bundle)
+        if let player = players[soundFileNameURL] {
+            
+            if !player.isPlaying {
+                player.prepareToPlay()
+                player.play()
+            } else {
+                
+                do {
+                    let duplicatePlayer = try AVAudioPlayer(contentsOf: soundFileNameURL)
+                    
+                    duplicatePlayer.delegate = self
+                    
+                    duplicatePlayers.append(duplicatePlayer)
+                    duplicatePlayer.prepareToPlay()
+                    duplicatePlayer.play()
+                    duplicatePlayer.numberOfLoops = loop ? -1 : 0
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            }
+        } else {
+            do {
+                let player = try AVAudioPlayer(contentsOf: soundFileNameURL)
+                players[soundFileNameURL] = player
+                player.prepareToPlay()
+                player.play()
+                player.numberOfLoops = loop ? -1 : 0
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func playMenuMusic() {
+       playSound(soundFileName: "Main-Menu-Soundtrack", loop: true)
+    }
+    
+    func playRatSound() {
+        playSound(soundFileName: "Rat-Sound", loop: false)
+    }
+    
+    func playLlamaSound() {
+        playSound(soundFileName: "Llama-Sound", loop: false)
+    }
+    
+    func playCatSound() {
+        playSound(soundFileName: "Cat-Sound", loop: false)
+    }
+    
+    func playFishSound() {
+        playSound(soundFileName: "Fish-Sound", loop: false)
+    }
+    
+    func playMeerkatSound() {
+        playSound(soundFileName: "Meerkat-Sound", loop: false)
+    }
+    
+    func pauseMusic() {
+        guard let bundle = Bundle.main.path(forResource: "TrilhaWor", ofType: "mp3")
+        else{ return }
+        let soundFileNameURL = URL(fileURLWithPath: bundle)
+        players[soundFileNameURL]?.stop()
+    }
+}
