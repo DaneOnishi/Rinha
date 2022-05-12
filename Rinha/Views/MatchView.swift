da2dd2cca1a25db60409f72bb7e77a07bd89454d
BrunoImai <bruno.y.l.imai@gmail.com>
2022-05-12 19:28:59 -0300

    Views Added
    - Background added
    - New components
    - MatchFondedView created

---
+++ Rinha/Views/MatchView.swift
@@ -0,0 +1,20 @@
+//
+//  MatchView.swift
+//  Rinha
+//
+//  Created by Bruno Imai on 12/05/22.
+//
+
+import SwiftUI
+
+struct MatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatchView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchView()
+    }
+}
